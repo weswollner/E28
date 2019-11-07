6232f0493cf0086bbfabc510750836ab6b0ee632
Wes Wollner <weswollner@gmail.com>
2019-11-07 17:34:39 -0500

    updates before product adds

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
+    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style lang='scss'>
+@import "./assets/css/zipfoods.scss";
+</style>
