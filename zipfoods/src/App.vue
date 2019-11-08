6dfc9f7c1920ca268adf4431316a8fb10f70284b
Wes Wollner <weswollner@gmail.com>
2019-11-07 19:28:41 -0500

    updates with products

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -2,13 +2,27 @@
   <div id="app">
     <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
     <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <show-featured></show-featured>
+    <show-products></show-products>
   </div>
 </template>
 
 <script>
+import ShowProducts from "./components/ShowProducts.vue";
+import ShowFeatured from "./components/ShowFeatured.vue";
+import { products } from "./products.js";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    ShowProducts,
+    ShowFeatured
+  },
+  data: function() {
+    return {
+      products: products
+    };
+  }
 };
 </script>
 
---
+++ zipfoods/src/components/ShowFeatured.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="featured">
+    <h2>Featured Products</h2>
+    <ul class="cleanList">
+      <li v-for="product in featuredProducts" :key="product.id">{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { products } from "./../products";
+export default {
+  name: "ShowFeatured",
+  computed: {
+    featuredProducts: function() {
+      function isMatch(product) {
+        return product.categories.includes(this);
+      }
+      return this.products.filter(isMatch, this.category);
+    }
+  },
+  data: function() {
+    return {
+      category: "snacks",
+      products: products
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
